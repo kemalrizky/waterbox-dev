a4954eacd73fca85cddf320133f7ad4687e4b56d
kemalrizky <kemalrizkyf.18@gmail.com>
2023-07-16 22:16:46 +0700

    adding config.h for macro def

---
+++ src/Config.h
@@ -0,0 +1,15 @@
+// Peripherals
+#define FLOW_SENSOR 15
+#define LED_RED 19
+#define LED_GREEN 13
+
+// Modem
+#define ROUTER_SSID "SSID"
+#define ROUTER_PASS "Password"
+
+// MQTT Server
+#define MQTT_SERVER "MQTT Server"
+
+// ESP32 Access Point
+#define AP_SSID "AP"
+#define AP_PASS "Password"

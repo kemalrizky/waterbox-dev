d700e0550d896b3ad37aac3ab8ca292f24e87d94
royyandzakiy <royyan.abdul@gmail.com>
2024-01-07 22:57:31 +0700

    refactor(main): - restructuring for ease of read. - adding read and publish intervals. - moving mqtt reconnection as seperate task to remove potential blocking

--- src/main.cpp
+++ src/main.cpp
@@ -5,57 +5,77 @@
 #include "waterflowsensor/SensorHandler.h"
 #include "led/LedHandler.h"
 
+LedHandler ledHandler;
+
 InternetHandler internetHandler;
 MqttHandler mqttHandler;
 OtaHandler otaHandler;
-SensorHandler sensorHandler;
-LedHandler ledHandler;
 
+SensorHandler sensorHandler;
 WaterFlowData wfd;
+
+void mqttReconnectTask(void *);
 
 void setup() {
   Serial.begin(115200);
+
   ledHandler.setup();
   ledHandler.turnOn(POWER_LED_PINOUT);
+
   internetHandler.init();
   mqttHandler.setup();
   otaHandler.init();
+  xTaskCreate(mqttReconnectTask, "mqttReconnectTask", 1024 * 1, NULL, 1, NULL);
+
   sensorHandler.init();
   sensorHandler.setCalibrationFactor(0.117);
 }
 
 void loop() {
   // update sensor data
-  sensorHandler.updateData(&wfd);
-
-  // Mantaining mqtt connection
-  if(!mqttHandler.isConnected()) {              
-    ledHandler.turnOff(CONNECTION_LED_PINOUT);
-
-    if(internetHandler.checkConnection() == InternetStatusCode::DISCONNECTED) {
-      if(internetHandler.connect()) {
-        if(mqttHandler.connect()) {
-          ledHandler.turnOn(CONNECTION_LED_PINOUT);
-        }
-      }                
-    }
-    else {
-      if(mqttHandler.connect()) {
-        ledHandler.turnOn(CONNECTION_LED_PINOUT);
-      }
-    }                   
-  } else {
+  if (millis() - sensorHandler.lastRead > sensorHandler.readInterval) {
+    sensorHandler.updateData(&wfd);
+    sensorHandler.lastRead = millis();
+  }
+  
   // send data
-    if(!wfd.isPublished) {
-      if(mqttHandler.publish("waterbox/W0002/flow_sensor/flowrate", wfd.flowRate) && 
-          mqttHandler.publish("waterbox/W0002/flow_sensor/volume", wfd.totalVolume)) {
-        ledHandler.blink(DATA_LED_PINOUT);
-        wfd.isPublished = true;
+  if (millis() - sensorHandler.lastPublish > sensorHandler.publishInterval) {
+    if(mqttHandler.isConnected()) {
+      if(!wfd.isPublished) {
+        if(mqttHandler.publish("waterbox/W0002/flow_sensor/flowrate", wfd.flowRate) && 
+            mqttHandler.publish("waterbox/W0002/flow_sensor/volume", wfd.totalVolume)) {
+          ledHandler.blink(DATA_LED_PINOUT);
+          wfd.isPublished = true;
+          sensorHandler.lastPublish = millis();
+        }
       }
     }
   }
 
   mqttHandler.loop();
-
   otaHandler.handleRequest();
+}
+
+void mqttReconnectTask(void * pv) {
+  while(1) {
+    // Mantaining mqtt connection
+    if(!mqttHandler.isConnected()) {              
+      ledHandler.turnOff(CONNECTION_LED_PINOUT);
+
+      if(internetHandler.checkConnection() == InternetStatusCode::DISCONNECTED) {
+        if(internetHandler.connect()) {
+          if(mqttHandler.connect()) {
+            ledHandler.turnOn(CONNECTION_LED_PINOUT);
+          }
+        }                
+      }
+      else {
+        if(mqttHandler.connect()) {
+          ledHandler.turnOn(CONNECTION_LED_PINOUT);
+        }
+      }                   
+    }
+
+    vTaskDelay(1000); 
+  }
 }
